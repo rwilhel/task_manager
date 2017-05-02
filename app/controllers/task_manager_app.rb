3f956cdcf901e3bf21bd4dcf74d3dc2d0a68ee8d
Rhonda <rwilhel@gmail.com>
2017-05-01 20:37:37 -0600

    Progress made on Task Manager functionality tutorial - CRUD via Sinatra

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -26,4 +26,14 @@ class TaskManagerApp < Sinatra::Base
     @task = Task.find(params[:id])
     erb :show
   end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
 end
